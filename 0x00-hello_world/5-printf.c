557ccef4a3d49d552b60528e1b9acac55af2a469
sosthehawk <orwarusosthenes@gmail.com>
2022-07-01 06:17:44 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description - this uses printf
+ * Return: integer
+ */
+int main(viod)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}	
