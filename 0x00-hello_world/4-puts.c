0a83538edcd25193a14331092402619ec532ca20
sosthehawk <orwarusosthenes@gmail.com>
2022-07-01 05:44:49 -0700

    name

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 /**
  * main - Entry point
  *
@@ -9,7 +8,6 @@
 int main(void)
 {   
 	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
+ 	return (0);
 
 }
