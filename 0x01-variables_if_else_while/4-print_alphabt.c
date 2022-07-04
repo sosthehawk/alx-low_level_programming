f4a9afe5cd3b9fb4fe7a7acd7c217eea25919e9e
sosthehawk <orwarusosthenes@gmail.com>
2022-07-04 07:05:32 -0700

    4 print alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return : returns 0
+ */
+int main(void)
+{
+	char alphabet, newline;
+
+	alphabet = 'a';
+	newline = '\n';
+	while (alphabet <= 'z')
+	{ 
+		if (alphabet == 'q' || alphabet == 'e')
+		{
+			alphabet++;
+		}
+		else
+		{
+			ptchar (alphabet);
+		alphabet++;
+		}
+	}
+	putchar (newline);
+	return (0);
+}	
