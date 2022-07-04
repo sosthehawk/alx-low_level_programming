fc48f808605e05b06dfa1108089625575ebf8be1
sosthehawk <orwarusosthenes@gmail.com>
2022-07-04 05:13:15 -0700

    Alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main -entry point
+ * Return: is an integer
+ */
+int main(void)
+{
+	char aiphabet_lowercase, alphabet_uppercase, newline;
+
+	alphabet_lowercase = 'a';
+	alphabet_uppercase = 'A';
+	newline = '\n';
+
+	while (alphabet_lowercase <= 'z')
+	{	
+		putchar(alphabet_lowercase);
+		alphabet_lowercase++;
+	}
+	while (alphabet_uppercase <= 'Z')
+	{
+		putchar(alphabet_uppercase <= 'Z')
+		alphabet_uppercase++;
+	}
+	putchar(newline);
+
+	return (0);
+}
