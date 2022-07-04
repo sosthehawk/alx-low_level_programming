4faa63d28b9e70777d3c88ac6c3a9a9079c03555
sosthehawk <orwarusosthenes@gmail.com>
2022-07-04 04:46:35 -0700

    alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: returns 0
+ */
+int main(void)
+{
+	char alphabet, newline;
+
+	alphabet = 'a';
+	newline = '\n';
+	while (aiphabet <= 'z')
+	{
+		putchar (alphabet);
+		aiphabet++;
+	}
+	putchar (newline);
+
+	return (0);
+}		
