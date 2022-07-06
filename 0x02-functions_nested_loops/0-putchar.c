6bd9da8ed6bc0c9419a891614acce7aeeaad157a
sosthehawk <orwarusosthenes@gmail.com>
2022-07-06 07:28:48 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ++ch)
+		_purchar(str[ch]);
+	_putchar('\n');
+	return (0);
+}
