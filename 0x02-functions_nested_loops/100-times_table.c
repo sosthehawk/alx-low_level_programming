5adbdf3898d32ced3e18e16cb8da19bc3433a8d8
sosthehawk <orwarusosthenes@gmail.com>
2022-07-07 14:06:17 +0300

    Create 100-times_table.c
    times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * print_times_table - print multiplication table up to n
+ * @n: integer argument
+ */
+void print_times_table(int n)
+{
+	int row;
+	int column;
+	int p;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (row = 0; row <= n; row++)
+		{
+			for (column = 0; column <= n; column++)
+			{
+				p = (row * column);
+				if (column == 0)
+					_putchar('0' + p);
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					if (p <= 9)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar('0' + p);
+					}
+					else if (p > 9 && p < 100)
+					{
+						_putchar(' ');
+						_putchar('0' + (p / 10));
+						_putchar('0' + (p % 10));
+					}
+					else if (p >= 100)
+					{
+						_putchar('0' + (p / 100));
+						_putchar('0' + ((p / 10) % 10));
+						_putchar('0' + (p % 10));
+					}
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
