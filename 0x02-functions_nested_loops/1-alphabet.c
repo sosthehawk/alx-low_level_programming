d85183a8ce2fbb5721945aa7160bd81156f97690
sosthehawk <orwarusosthenes@gmail.com>
2022-07-07 02:34:02 -0700

    alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the lower case
+ * Return: always 0 (success)
+ */
+void print_alphabet(void)
+{
+	char n;
+
+	n = 'a'
+	while (n <= 'z')
+	{
+		_putchar(n);
+		n++;
+	}
+	_putchar('\n');
+}	
