c1269d3ef649f4d6d01e91970d3f730c6c0218bf
sosthehawk <orwarusosthenes@gmail.com>
2022-07-15 17:03:20 +0300

    Create 101-print_number.c
    Task 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *print_number - prints an integer without using long
+ *@n: integer
+ *You can only use _putchar function to print
+ *You are not allowed to use long
+ *You are not allowed to use arrays or pointers
+ *You are not allowed to hard-code special values
+ *Return: nothing
+ */
+void print_number(int n)
+{
+unsigned int i = 0;
+
+if (n < 0)
+{
+_putchar('-');
+n = -n;
+}
+i = n;
+if (i / 10)
+print_number(i / 10);
+_putchar(i % 10 + '0');
+}
