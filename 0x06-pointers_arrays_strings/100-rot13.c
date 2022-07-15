b3f430ee76121cbf23a77eaca79d2ccb1c5b7651
sosthehawk <orwarusosthenes@gmail.com>
2022-07-15 16:59:37 +0300

    Create 100-rot13.c
    Task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ *rot13 - encodes a string using rot13
+ *@s: string
+ *Return: nothing
+ */
+char *rot13(char *s)
+{
+int one[53] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+	       'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+	       'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+	       'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+	       'W', 'X', 'Y', 'Z'};
+int two[53] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+	       'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+	       'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+	       'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+	       'J', 'K', 'L', 'M'};
+int i, j;
+j = 0;
+for (j = 0; s[j] != '\0'; j++)
+{
+i = 0;
+while (one[i] != '\0' && s[j] != one[i])
+i++;
+
+if (s[j] == one[i])
+s[j] = two[i];
+}
+
+return (s);
+}
